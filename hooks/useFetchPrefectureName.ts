6dd3c89edf74077bf1eb157d4f270d332ddb392c
Banri Yasui <69246274+jomspk@users.noreply.github.com>
2023-09-02 16:21:14 +0900

    update: 都道府県名情報のフェッチ

---
+++ hooks/useFetchPrefectureName.ts
@@ -0,0 +1,28 @@
+export const useFetchPrefectureName = async () => {
+  try {
+    const res = await fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+      method: "GET",
+      headers: {
+        "X-API-KEY": process.env.NEXT_PUBLIC_APIKEY,
+      },
+    });
+
+    const data = await res.json();
+    const prefectures = [...data.result];
+
+    console.log("This is prefectures: ", prefectures);
+
+    return {
+      props: {
+        prefectures,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        prefectures: [],
+        error: JSON.stringify(error),
+      },
+    };
+  }
+};
