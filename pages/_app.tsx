b534c365a632c4b1c0d6cebbca2723028d529a0e
Banri Yasui <69246274+jomspk@users.noreply.github.com>
2023-09-09 11:29:24 +0900

    add: ヘッダーの作成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "@/components/organisms/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>都道府県別人口推移グラフ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
