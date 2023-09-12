593d7c3e3a294ff2e7caa33f608865dc93239c45
Banri Yasui <69246274+jomspk@users.noreply.github.com>
2023-09-12 22:08:27 +0900

    add: HeaderとPrefectureのテストケースを作成

---
+++ components/molecules/SelectGraphData.test.tsx
@@ -0,0 +1,46 @@
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { render, screen, fireEvent } from '@testing-library/react';
+import SelectGraphData from './SelectGraphData';
+
+describe('SelectGraphDataコンポーネントのテスト', () => {
+  it('正しくレンダリングされる', () => {
+    // Arrange
+    const handleClick = jest.fn();
+    render(<SelectGraphData selectData={handleClick} />);
+
+    //Assert
+    expect(screen.getByRole('option', { name: '総人口' })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: '若年人口' })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: '生産年齢人口' })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: '老年人口' })).toBeInTheDocument();
+  });
+
+  it('正しく選択できる', async () => {
+    // Arrange
+    const handleClick = jest.fn();
+    render(<SelectGraphData selectData={handleClick} />);
+
+    //Act
+    await userEvent.selectOptions(screen.getByRole('combobox'), '若年人口');
+
+    //Assert
+    expect(screen.getByRole('option', { name: '総人口' }).selected).toBe(false);
+    expect(screen.getByRole('option', { name: '若年人口' }).selected).toBe(true);
+    expect(screen.getByRole('option', { name: '生産年齢人口' }).selected).toBe(false);
+    expect(screen.getByRole('option', { name: '老年人口' }).selected).toBe(false);
+  });
+
+  it('クリック時にonClickイベントハンドラがトリガーされる', () => {
+    //Arrange
+    const handleClick = jest.fn();
+    render(<SelectGraphData selectData={handleClick} />);
+
+    //Act
+    const selectElement = screen.getByRole('combobox');
+    fireEvent.change(selectElement, { target: { value: 'youthPopulation' } });
+
+    //Assert
+    expect(handleClick).toHaveBeenCalled();
+  });
+});
